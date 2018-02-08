023780fb4f386d1f781dcaa79be255547479a2ae
Jin Zhu <jinz1@andrew.cmu.edu>
2018-02-08 17:09:18 -0500

    mnist 1 layer, class

---
+++ TensorflowClass/mnistSingleLayer.py
@@ -0,0 +1,64 @@
+# Lenet
+# https://github.com/tiagofrepereira2012/examples.tensorflow/blob/master/examples/tensorflow/lenet.py
+# VAE
+# https://github.com/allenovo/conditional_vae/blob/master/vae.py
+# MNIST
+# https://github.com/tensorflow/tensorflow/blob/r1.1/tensorflow/examples/tutorials/mnist/mnist_softmax.py
+# Tensorboard:
+# group layers
+import tensorflow as tf
+
+
+class simpleNN():
+    def __init__(self):
+        numInputs = 28*28
+        numClass = 10
+        learning_rate = 0.0001
+        with tf.variable_scope('simpleNN'):
+           self.x= tf.placeholder(tf.float32, [None, numInputs], name = 'input')
+           # one hot encode labels
+           # self.labels  [batch_size, num_classes]
+           self.labels =  tf.placeholder(tf.float32, [None], name = 'output')
+           one_hot_labels = tf.one_hot(self.labels, numClass)
+           # one fullly connected layer
+           self.W_fc0 = tf.Variable(tf.truncated_normal(shape = (numInputs, numClass), mean = 0, stddev = 1))
+           self.b_fc0 = tf.Variable(tf.zeros(numClass))
+           # [batch_size, num_classes]
+           self.logits = tf.matmul(self.x, self.W_fc0) + self.b_fc0
+           # define loss and optimizer
+           # we call softmax_cross_entropy_with_logits on tf.matmul(x, W) + b),
+           # because this more numerically stable function internally computes the softmax activation.
+
+           # y = tf.nn.softmax(tf.matmul(x, W) + b)
+           # y_ = tf.placeholder(tf.float32, [None, 10])
+           # cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+           cross_entropy = tf.nn.softmax_cross_entropy_with_logits(self.labels,self.logits)
+           self.loss = tf.reduce_mean(cross_entropy)
+           self.opt = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
